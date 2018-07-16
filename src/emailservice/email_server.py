74395557e5b47f739fb33e13de1388cb6c4d734e
Ahmet Alp Balkan <ahmetb@google.com>
2018-07-16 15:09:51 -0700

    refactor fix after profiler patch
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- src/emailservice/email_server.py
+++ src/emailservice/email_server.py
@@ -29,7 +29,7 @@ import demo_pb2_grpc
 # try:
 #     googleclouddebugger.enable(
 #         module='emailserver',
-#         version='[VERSION]'
+#         version='[1.0.0]'
 #     )
 # except:
 #     pass
